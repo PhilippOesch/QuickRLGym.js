5767f3ee24889c5f8921a21a1540b3d2ec4a00a1
Philipp Oeschger <kontakt@philipp-oeschger.de>
2022-12-27 19:50:06 +0100

    Update argMax function

--- coreLibary/src/Utils/MathUtils.ts
+++ coreLibary/src/Utils/MathUtils.ts
@@ -1,8 +1,12 @@
 module MathUtils {
     export function argMax(array: number[]): number {
+        if (!array || array.length < 1) {
+            return -1;
+        }
+
         let maxIdx: number = 0;
         let max: number = array[0];
-        for (let i = 0; i < array.length; i++) {
+        for (let i = 1; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
                 maxIdx = i;
