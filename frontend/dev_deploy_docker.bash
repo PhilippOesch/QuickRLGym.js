76fcae215db85516bd7cffd209014752e04961f2
Philipp Oeschger <kontakt@philipp-oeschger.de>
2023-05-26 22:53:27 +0200

    fix actions finaly

--- frontend/dev_deploy_docker.bash
+++ frontend/dev_deploy_docker.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+#For Testing
 echo "start deployment"
 docker buildx build -t 192.168.0.135:5000/quickrljs:latest . --platform linux/amd64
 docker push 192.168.0.135:5000/quickrljs:latest  
